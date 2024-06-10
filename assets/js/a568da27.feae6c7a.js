4c04ddbf71578ad36286daa6ea2e2a1331ed9be3
gan11a <ivan.tulskuy@gmail.com>
2024-06-11 02:02:49 +0400

    Deploy website - based on 4f2744006e4c539ea59ffc3494542dd9892090ad

---
+++ assets/js/a568da27.feae6c7a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvault=self.webpackChunkvault||[]).push([[6667],{3040:t=>{t.exports=JSON.parse('{"metadata":{"permalink":"/Vault/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.e0f7624a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvault=self.webpackChunkvault||[]).push([[1903],{1912:l=>{l.exports=JSON.parse('{"blogBasePath":"/Vault/blog","blogTitle":"Blog"}')}}]);
