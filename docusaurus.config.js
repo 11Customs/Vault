fb8ec9f77e8338c1f9abe921e7200930fb0515df
gan11a <ivan.tulskuy@gmail.com>
2024-06-26 03:36:29 +0400

    chaged url again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'vault.11customs.com',
+  url: 'https://vault.11customs.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
