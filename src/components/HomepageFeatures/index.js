9f9288c569f82d1a9c67d6214101ecb1a6e46e4c
gan11a <ivan.tulskuy@gmail.com>
2024-07-10 20:23:44 +0400

    Homepage changes #1

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,12 +4,12 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Knowledge Accumulated',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The preservation of knowledge is an important task that we as humans face, therefore this website. 
+        You can read more about the project here.
       </>
     ),
   },
