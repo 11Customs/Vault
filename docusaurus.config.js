f3b1938710405338c768a5b4a24264e8a8343b24
gan11a <ivan.tulskuy@gmail.com>
2024-07-12 12:58:51 +0400

    Homepage changes 10

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,31 +35,31 @@ const config = {
     locales: ['en'],
   },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
+  // presets: [
+  //  [
+  //    'classic',
+  //    /** @type {import('@docusaurus/preset-classic').Options} */
+  //    ({
+  //      docs: {
+  //        sidebarPath: './sidebars.js',
+  //        // Please change this to your repo.
+  //        // Remove this to remove the "edit this page" links.
+  //        editUrl:
+  //          'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+  //      },
+  //      blog: {
+  //        showReadingTime: true,
+  //        // Please change this to your repo.
+  //        // Remove this to remove the "edit this page" links.
+  //        editUrl:
+  //          'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+  //      },
+  //      theme: {
+  //        customCss: './src/css/custom.css',
+  //      },
+  //    }),
+  //  ],
+  //],
 
   themeConfig:
 
